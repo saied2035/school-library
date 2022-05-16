54065a5c747bffaae5a2a7f34fa0b011a8da1db6
unknown <saied2421998@gmail.com>
2022-05-16 12:03:50 +0200

    Create Person class

---
+++ Person.rb
@@ -0,0 +1,21 @@
+class Person
+	attr_reader :id
+	attr_accessor :name , :age
+
+	def initialize age, name = "Unknown", parent_permission = true
+	  @name = name
+	  @age = age
+	  @parent_permission = parent_permission
+    end
+
+    def is_of_age?
+    	return true if @age >= 18
+    	return false
+    end
+    private :is_of_age?
+
+    def can_use_services?
+    	return true if @age >=18 || parent_permission == true
+    	return false
+    end	
+end	
