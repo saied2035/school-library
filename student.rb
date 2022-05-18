2bdacf1001a7c988bf02ccd6d764868097d241af
unknown <saied2421998@gmail.com>
2022-05-18 13:39:34 +0200

    set up association between classroom and student classes

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require './person'
 # child class
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
-    @classroom = classroom
+   attr_reader :classroom
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+    @classroom = self.add_classroom classroom
+    super(age, name, parent_permission: parent_permission)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
+  def add_classroom classroom 
+      if classroom.class == Classroom
+        @classroom = classroom
+        classroom.add_student self
+     end
+  end  
 end
