098d00ab15ec0e02db4c0722ac4218a8474d83ae
unknown <saied2421998@gmail.com>
2022-05-18 15:30:21 +0200

    set up association between Person and Rental classes

--- rental.rb
+++ rental.rb
@@ -1,16 +1,23 @@
 class Rental
 	attr_reader :book
 	attr_accessor :date
-	def initialize(date, book) 
+	def initialize(date, book, person) 
 		@date = date
 		@book = self.book_rentaled book
+		@person = self.person_rental person
 	end
 	def book_rentaled book 
 		if book.class == Book
 			@book = book
 			book.rental_book self
 			return @book 
-		end 
-        
-	end	
+		end   
+	end
+		def person_rental person 
+		if person.class == Person
+			@person = person
+			person.person_rentals self
+			return @person 
+		end   
+	end		
 end	
