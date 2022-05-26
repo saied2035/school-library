384cf937de6439d5d4d05bd9299e7328902a1c78
bushmusi <bushera.mestofa@gmail.com>
2022-05-26 15:10:21 +0300

    Add load and create people list

--- create_classes.rb
+++ create_classes.rb
@@ -2,9 +2,11 @@ require './teacher'
 require './student'
 require './book_storage'
 require './rental_storage'
+require './people_store'
+require 'json'
 # create classes
 class CreateClasses
-  attr_reader :book_storage, :people_list, :rental_storage
+  attr_reader :book_storage, :people_list, :rental_storage, :file
 
   def initialize
     @book_storage = BookStorage.new
@@ -12,6 +14,26 @@ class CreateClasses
     @rental_storage = RentalStorage.new
     @book_storage.presaved_books
     @rental_storage.presaved_rentals(@book_storage.book_list, @people_list)
+    load_student
+    load_teacher
+  end
+
+  def load_student
+    @file = File.read('people.json')
+    hash_data = JSON.parse(@file)
+    hash_data['student'].each do |item|
+      student = Student.new(item['age'], nil, item['name'], parent_permission: item['parent_permission'])
+      @people_list.push({ value: student, type: 'Student' })
+    end
+  end
+
+  def load_teacher
+    @file = File.read('people.json')
+    hash_data = JSON.parse(@file)
+    hash_data['teacher'].each do |item|
+      teacher = Teacher.new(item['age'], item['specialization'], item['name'])
+      @people_list.push({ value: teacher, type: 'Teacher' })
+    end
   end
 
   def add_book(title, author)
@@ -19,11 +41,15 @@ class CreateClasses
   end
 
   def add_student(age, name, parent_permission)
+    @student_list = StudentStore.new(age, name, parent_permission)
+    @student_list.store_student
     student = Student.new(age, nil, name, parent_permission: parent_permission)
     @people_list.push({ value: student, type: 'Student' })
   end
 
   def add_teacher(age, name, specialization)
+    @teacher_list = TeacherStore.new(age, specialization, name)
+    @teacher_list.store_teacher
     teacher = Teacher.new(age, specialization, name)
     @people_list.push({ value: teacher, type: 'Teacher' })
   end
