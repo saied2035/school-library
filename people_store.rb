66a3325a113e4160f10977568045bf553f8dd71d
bushmusi <bushera.mestofa@gmail.com>
2022-05-26 15:11:10 +0300

    Optimise code

--- people_store.rb
+++ people_store.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'create_classes'
 
 # Store people in json file
 class StudentStore
@@ -11,24 +10,14 @@ class StudentStore
     @age = age
     @name = name
     @permission_value = permission_value
-    @file = File.open('people.json', 'a')
     @create_class = CreateClasses.new
   end
 
   def store_student
     file = File.read('people.json')
     hash_data = JSON.parse(file)
-    hash_data['student'].push({"name": @name, "age": @age, "parent_permission": @permission_value})
+    hash_data['student'].push({ "name": @name, "age": @age, "parent_permission": @permission_value })
     File.write('people.json', JSON.pretty_generate(hash_data))
-  end
-
-  def load_people
-    file = File.read('people.json')
-    hash_data = JSON.parse(file)
-    student_list = hash_data['student']
-    teacher_list = hash_data['teacher']
-    student_list.each { |item| @create_class.add_book(item['age'], item['name'], item['parent_permission']) }
-    teacher_list.each { |item| @create_class.add_book(item['age'], item['name'], item['specialization']) }
   end
 end
 
@@ -36,7 +25,7 @@ end
 class TeacherStore
   attr_accessor :age, :name, :specialization
 
-  def initialize(age, name, specialization)
+  def initialize(age, specialization, name)
     @age = age
     @name = name
     @specialization = specialization
@@ -45,7 +34,7 @@ class TeacherStore
   def store_teacher
     file = File.read('people.json')
     hash_data = JSON.parse(file)
-    hash_data['teacher'].push({ "name": @name, "age": @age, "specialization": @specialization })
+    hash_data['teacher'].push({ "age": @age, "specialization": @specialization, "name": @name })
     File.write('people.json', JSON.pretty_generate(hash_data))
   end
 end
